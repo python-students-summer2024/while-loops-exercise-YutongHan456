30facd67a4bbb0b22c6561f47422596279204527
YutongHan456 <yh4980@nyu.edu>
2024-06-20 00:37:37 -0400

    while_loop

---
+++ while_loop_flag_version.py
@@ -0,0 +1,19 @@
+def get_starting_number():
+    while True:
+        number = input("How many bottles of beer on the wall?")
+        if number.isnumeric() == True and int(number)>=1:
+            return int(number)
+
+
+def sing(number):
+    num = 0
+    while True:
+        num_befor = number - num
+        num_after = num_befor - 1
+        if num == number - 1:
+            print(f"{num_befor} bottles of beer on the wall, {num_befor} bottles of beer.")
+            print(f"Take one down, pass it around, no more bottles of beer on the wall!")
+            return
+        print(f"{num_befor} bottles of beer on the wall, {num_befor} bottles of beer.")
+        print(f"Take one down, pass it around, {num_after} bottles of beer on the wall.\n")
+        num += 1
